25fcd14f8f39b4389624381f44756d2014d94240
ZXY0827 <yang.zixuan@northeastern.edu>
2023-04-02 18:29:05 -0700

    a8

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -4,14 +4,14 @@ export default [
         "handle": "@spacex", "time": "2h", "image": "spacex.jpg",
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345, "disliked": true, "dislikes": 1, "replies": 123, "retuits": 432
+        "liked": true, "likes": 2345, "disliked": false, "dislikes": 1, "replies": 123, "retuits": 432
     },
     {
         "_id": "234", "topic": "Traffic", "username": "The Boring Company",
         "handle": "@boringcompany", "time": "2h", "image": "boring.png",
         "title": "The Boring Company fixing traffic",
         "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-        "liked": true, "likes": 2345, "disliked": true, "dislikes": 10, "replies": 321, "retuits": 432
+        "liked": false, "likes": 2345, "disliked": true, "dislikes": 10, "replies": 321, "retuits": 432
     },
     {
         "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
