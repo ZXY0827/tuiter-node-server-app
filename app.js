40125026678d9aea99d4359a8b6c0fed5aa2bcff
ZXY0827 <yang.zixuan@northeastern.edu>
2023-04-09 12:01:22 -0700

    a9

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
+
+import dotenv from 'dotenv';
+dotenv.config();
 // const express = require('express')
 // mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 // mongoose.connect('mongodb+srv://christina:YANGfan123@cluster0.hkmugao.mongodb.net/tuiter?retryWrites=true&w=majority');
